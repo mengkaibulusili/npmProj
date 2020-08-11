eb64fd1b687e28d50f5bdf78e6745d391c86cbf0
mengkaibulusili <734449600@qq.com>
2020-08-11 08:30:11 +0800

    dev20200811

--- shopServer/vips/models.py
+++ shopServer/vips/models.py
@@ -3,45 +3,61 @@ import uuid
 import scriptTools.timeTools as tTools
 
 charlen = 50
+
+
 # Create your models here.
 # 新增会员
 class VipsInfo(models.Model):
-  vipid = models.AutoField("vipid",primary_key=True, auto_created=True)
-  vipuuid = models.CharField("vipuuid",default=uuid.uuid1,blank=True,max_length=charlen)
-  createtime = models.CharField("createtime",default=tTools.dateStdTime, max_length=charlen,blank=True)
-  name = models.CharField("name",max_length=charlen)
-  telephone = models.CharField("telephone",max_length=charlen)
-  birthday = models.CharField("birthday",max_length=charlen)
-  rechangetype = models.CharField("rechangetype",max_length=charlen)
-  summoney = models.CharField("summoney",max_length=charlen)
-  staffuuid = models.CharField("staffuuid",max_length=charlen)
-  staff = models.CharField("staff",max_length=charlen)
-  remainmoney = models.CharField("remainmoney",max_length=charlen)
+  vipid = models.AutoField("vipid", primary_key=True, auto_created=True)
+  vipuuid = models.CharField("vipuuid", default=uuid.uuid1, blank=True, max_length=charlen)
+  createtime = models.CharField("createtime", default=tTools.dateStdTime, max_length=charlen, blank=True)
+  summoney = models.CharField("summoney", default="0", max_length=charlen)
+  remainmoney = models.CharField("remainmoney", default="0", max_length=charlen)
+ 
+  name = models.CharField("name", max_length=charlen)
+  telephone = models.CharField("telephone", unique=True, max_length=charlen)
+  birthday = models.CharField("birthday", max_length=charlen)
+  staffuuid = models.CharField("staffuuid", max_length=charlen)
+  staff = models.CharField("staff", max_length=charlen)
+
 
 # 新购项目
 class VipsProj(models.Model):
-  vipprojid = models.AutoField("vipprojid",primary_key=True)
-  vipprojuuid = models.CharField("vipprojuuid",default=uuid.uuid1,blank=True,max_length=charlen)
-  createtime = models.CharField("createtime",default=tTools.dateStdTime, max_length=charlen,blank=True)
+  vipprojid = models.AutoField("vipprojid", primary_key=True)
+  vipprojuuid = models.CharField("vipprojuuid", default=uuid.uuid1, blank=True, max_length=charlen)
+  createtime = models.CharField("createtime", default=tTools.dateStdTime, max_length=charlen, blank=True)
   vipprojuuid = models.CharField("vipprojuuid", max_length=charlen)
-  vipprojname = models.CharField("vipprojname",max_length=charlen)
-  vipprojprice = models.CharField("vipprojname",max_length=charlen)
-  staffuuid = models.CharField("staffuuid",max_length=charlen)
-  staff = models.CharField("staff",max_length=charlen)
-  state = models.CharField("state",max_length=charlen)
-  useTime = models.CharField("useTime",max_length=charlen)
+  vipprojname = models.CharField("vipprojname", max_length=charlen)
+  vipprojprice = models.CharField("vipprojprice", max_length=charlen)
+  staffuuid = models.CharField("staffuuid", max_length=charlen)
+  staff = models.CharField("staff", max_length=charlen)
+  state = models.CharField("state", max_length=charlen)
+  useTime = models.CharField("useTime", max_length=charlen)
+
 
 # 新增购物
 class VipsConsume(models.Model):
-  vipconsumeid = models.AutoField("vipconsumeid",primary_key=True)
-  vipconsumeuuid = models.CharField("vipconsumeuuid",default=uuid.uuid1,blank=True,max_length=charlen)
-  createtime = models.CharField("createtime",default=tTools.dateStdTime, max_length=charlen,blank=True)
+  vipconsumeid = models.AutoField("vipconsumeid", primary_key=True)
+  vipconsumeuuid = models.CharField("vipconsumeuuid", default=uuid.uuid1, blank=True, max_length=charlen)
+  createtime = models.CharField("createtime", default=tTools.dateStdTime, max_length=charlen, blank=True)
   # sp  的 uuid
   gooduuid = models.CharField("gooduuid", max_length=charlen)
-  goodname = models.CharField("goodname",max_length=charlen)
-  goodmoney = models.CharField("goodmoney",max_length=charlen)
-  staffuuid = models.CharField("staffuuid",max_length=charlen)
-  staff = models.CharField("staff",max_length=charlen)
+  goodname = models.CharField("goodname", max_length=charlen)
+  goodmoney = models.CharField("goodmoney", max_length=charlen)
+  staffuuid = models.CharField("staffuuid", max_length=charlen)
+  staff = models.CharField("staff", max_length=charlen)
+
+
+# 会员充值
+class VipsRechange(models.Model):
+  viprechangeid = models.AutoField("viprechangeid", primary_key=True, auto_created=True)
+  viprechangeuuid = models.CharField("viprechangeuuid", default=uuid.uuid1, blank=True, max_length=charlen)
+  createtime = models.CharField("createtime", default=tTools.dateStdTime, max_length=charlen, blank=True)
+  vipname = models.CharField("vipname", max_length=charlen)
+  viptelephone = models.CharField("viptelephone", max_length=charlen)
+  rechangemoney = models.CharField("rechangemoney", max_length=charlen)
+  staffuuid = models.CharField("staffuuid", max_length=charlen)
+  staff = models.CharField("staff", max_length=charlen)
 
 
 # python manage.py shell
--- shopServer/vips/views.py
+++ shopServer/vips/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.http import request
+
 
 # Create your views here.
 def usefucbyname(request, fucname):
   return eval(fucname)(request)
 
+
 # 序列化数据库数据
 # python manage.py shell
 # from django.core import serializers
 # data = serializers.serialize('json', vips.VipsInfo.objects.all(), fields=('name','tele'))
+
+
+def createVip(request):
+  request.GET.get("id")
+  return HttpResponse("")
